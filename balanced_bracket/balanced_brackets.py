252b28be7968e55f5e89a1290cdde910d9f1da43
Cilpa <cilpavinu@gmail.com>
2021-04-03 10:32:16 +0530

    updated

--- balanced_bracket/balanced_brackets.py
+++ balanced_bracket/balanced_brackets.py
@@ -1,7 +1,12 @@
-open_list = ["[","{","("]
-close_list = ["]","}",")"]
+
+# open_list = ["[","{","("]
+# close_list = ["]","}",")"]
 
 def check(myStr):
+    
+    open_list = ["[","{","("]
+    close_list = ["]","}",")"]
+
     stack = []
     for i in myStr:
         if i in open_list:
@@ -11,11 +16,14 @@ def check(myStr):
             if ((len(stack)>0) and (open_list[pos] == stack[len(stack)-1])):
                 stack.pop()
             else:
+                print("False")
                 return False
     
     if len(stack) == 0:
+        print("True")
         return True
     else:
+        print("False")
         return False
         
 
